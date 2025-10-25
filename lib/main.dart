9ad5f979128f92269b321088ce857630b081562c
JoseCarlosCueva <148024202+JoseCarlosCueva@users.noreply.github.com>
2025-10-24 20:28:20 -0600

    ESTE ES MI PRIMER COMMIT JCUEVAS

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'JOse Danile Diaz'),
+      //Josecarloscueva
     );
   }
 }
