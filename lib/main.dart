9dc78860173a2abe74548486402298f561a61923
Oscar-Murillo <oscaralexandermurillo500@gmail.com>
2025-10-31 20:04:47 -0600

    Configuracion de Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 // Esta es la rama Dev
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
